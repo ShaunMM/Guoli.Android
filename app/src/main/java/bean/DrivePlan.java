b962cb9e2ee5dba7a61e00da509c018f75839d9e
mao <972850173@qq.com>
2016-11-30 18:07:12 +0800

    行车计划实体类

---
+++ app/src/main/java/bean/DrivePlan.java
@@ -0,0 +1,27 @@
+package bean;
+
+import java.util.Date;
+
+/**
+ * Created by mao on 2016/11/30.
+ */
+
+public class DrivePlan {
+    private int id;
+    private String LineName;
+    private String TrainCode;
+    private String LocoType;
+    private String DriverNo;
+    private String DriverName;
+    private String ViceDriverNo;
+    private String ViceDriverName;
+    private String StudentNo;
+    private String StudentName;
+    private String OtherNo1;
+    private String OtherName1;
+    private String OtherNo2;
+    private String OtherName2;
+    private Date AttendTime;
+    private Date StartTime;
+
+}
